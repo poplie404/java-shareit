bb759bf094f4d86c50a206feadca7c00a3d89e8b
Aleks Zaripov <lexazarip2005@gmail.com>
2025-10-29 20:29:38 +0300

    исправление ошибок с git

--- src/main/java/ru/practicum/shareit/ShareItApplication.java
+++ src/main/java/ru/practicum/shareit/ShareItApplication.java
@@ -2,9 +2,10 @@ package ru.practicum.shareit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "ru.practicum.shareit")
 public class ShareItApplication {
     public static void main(String[] args) {
         SpringApplication.run(ShareItApplication.class, args);
     }
 }
+
